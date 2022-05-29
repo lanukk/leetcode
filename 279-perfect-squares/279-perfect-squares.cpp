43c17eb00bed5eac43ed3ee64744cdcff79a3766
Kunal Bisht <bishtkunal40@gmail.com>
2022-05-29 12:53:18 +0530

    Time: 514 ms (13.95%), Space: 12 MB (18.83%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     
-    int dp[10002][200];
+    int dp[10002][128];
     
-    int recursion(vector<int>&nums, int n, int i){
+    int recursion(int n, int i){
         if(n == 0)
             return 0;
         
-        if(n - nums[i] < 0)
+        if(n - i * i < 0)
             return 1e5;
         
         if(dp[n][i] != -1)
             return dp[n][i];
         
-        return dp[n][i] =  min(1 + recursion(nums, n - nums[i], i), recursion(nums, n, i + 1));        
+        return dp[n][i] =  min(1 + recursion(n - i * i, i), recursion(n, i + 1));        
     }
     
     int numSquares(int n) {
-        vector<int> nums;
-        int p = 1;
-        while(p*p <= 1e4){
-            nums.push_back(p * p);
-            p++;
-        }
-        
         for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= (int)nums.size(); j++){
+            for(int j = 0; j < 128; j++){
                 dp[i][j] = -1;
             }
         }
         
-        int ans = recursion(nums, n, 0);
+        int ans = recursion(n, 1);
         if(ans == 1e5)
             ans = -1;
         
-        return recursion(nums, n, 0);
+        return ans;
     }
 };
