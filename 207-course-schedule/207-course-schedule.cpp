5607d9c3f2e92d0307d9759816a7bc6e499543e0
Kunal Bisht <bishtkunal40@gmail.com>
2022-07-15 12:25:18 +0530

    Time: 21 ms (92.01%), Space: 14.6 MB (30.41%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
+    
+    bool dfs(int v, vector<vector<int>> & adj, vector<int> & color){
+        color[v] = 1;
+        bool ans = true;
+        for(int u : adj[v]){
+            if(color[u] == 0){
+                ans = (ans & dfs(u, adj, color));
+            } else if(color[u] == 1){
+                ans = false;
+            }
+        }
+        color[v] = 2;
+        
+        return ans;
+    }
+    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for(auto x : prerequisites)
             adj[x[1]].push_back(x[0]);
         
-        vector<int> inDegree(numCourses);
+        vector<int> color(numCourses);
+        bool ans = true;
         for(int i = 0; i < numCourses; i++){
-            for(int v : adj[i])
-                inDegree[v]++;
-        }
-        
-        queue<int> q;
-        for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0){
-                q.push(i);
+            if(color[i] == 0){
+                ans = (ans & dfs(i, adj, color));
             }
         }
         
-        while(!q.empty()){
-            int u = q.front();
-            numCourses--;
-            q.pop();
-            
-            for(int i : adj[u]){
-                inDegree[i]--;
-                if(inDegree[i] == 0){
-                    q.push(i);
-                }
-            }
-        }
-        
-        return numCourses == 0;        
+        return ans;
     }
 };
