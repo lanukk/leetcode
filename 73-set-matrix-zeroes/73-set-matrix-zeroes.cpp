8e2ef1fefa1efe8eb36e106475908a836f23a6ca
Kunal Bisht <bishtkunal40@gmail.com>
2022-07-21 08:28:52 +0530

    Time: 104 ms (5.70%), Space: 13 MB (95.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -5,28 +5,53 @@ public:
         int n = (int)matrix.size();
         int m = (int)matrix[0].size();
         
-        vector<bool> r(n, false), c(m, false);
-        
+        bool row = false, col = false;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+            if(matrix[i][0] == 0)
+                col = true;
+        }
+        
+        for(int i = 0; i < m; i++){
+            if(matrix[0][i] == 0)
+                row = true;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] == 0){
-                    r[i] = true;
-                    c[j] = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
         
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(r[i] == true || c[j] == true){
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+    // cout<<row<<" "<<col<<"\n";        
+        
+        if(row){
+            for(int i = 0; i < m; i++)
+                matrix[0][i] = 0;
+        }
+        if(col){
+            for(int i = 0; i < n; i++)
+                matrix[i][0] = 0;
+        }
+        
+        
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 cout<<matrix[i][j]<<" ";
             }
             cout<<"\n";
-        }       
-    
+        }
         
     }
 };
