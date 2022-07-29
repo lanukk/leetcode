aab1fcf0a1398fa69b1dec5c9f1b8917c37078f1
Kunal Bisht <bishtkunal40@gmail.com>
2022-07-30 03:00:39 +0530

    Time: 21 ms (69.60%), Space: 19.8 MB (81.60%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     
-    int solve(vector<int>& a, bool equal){
+    int findSolve(vector<int> &a, bool equal){
+        int n = (int) a.size();
         int ans = 0;
-        int n = (int)a.size();
-        
         for(int i = 0; i < n; i++){
+            int curr = a[i];
             int j = i + 1;
             int sum = 0;
-            
             while(j < n){
+                if(equal && a[i] == a[j])
+                    break;
                 if(a[i] < a[j])
                     break;
-                if(a[i] == a[j] && equal)
-                    break;
+                sum += a[j];
                 j++;
             }
             if(j == n)
                 break;
-            if(j - i == 1)
-                continue;
             
-            for(int k = i + 1; k < j; k++)
-                sum += a[k];
-            
-            // cout<<i<<" "<<j<<" "<<a[i]<<" "<<sum<<"\n";
-            ans += (j - i - 1) * a[i] - sum;
+            ans += (a[i] * (j - i - 1)) - sum;
             i = j - 1;
         }
         return ans;
     }
     
     int trap(vector<int>& height) {
-        int ans = solve(height, 1);
-        reverse(height.begin(), height.end());
-        ans += solve(height, 0);
+        int ans = 0;
         
-        return ans;
+        ans = findSolve(height, 1);
+        reverse(height.begin(), height.end());
+        ans += findSolve(height, 0);
+        
+        return ans;        
     }
 };
