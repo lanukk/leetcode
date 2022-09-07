4ba53267110abb67b3e671434bbeb38b466633ba
Kunal Bisht <bishtkunal40@gmail.com>
2022-09-07 12:05:50 +0530

    Time: 501 ms (57.68%), Space: 117 MB (89.07%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,34 +1,41 @@
 class MedianFinder {
     priority_queue<int, vector<int>> left;
     priority_queue<int, vector<int>, greater<int>> right;
+    
 public:
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(left.size() == 0 || num < left.top())
-            left.push(num);
-        else
-            right.push(num);
-        
-        if((int)left.size() == (int)right.size() + 2){
+        left.push(num);
+        if((int)right.size() > 0 && right.top() < left.top()){
             right.push(left.top());
             left.pop();
         }
-        if((int)right.size() == (int)left.size() + 2){
+        // if(left.size() == 0 || num < left.top())
+        //     left.push(num);
+        // else
+        //     right.push(num);
+        
+        if((int)right.size() - (int)left.size() >= 2){
             left.push(right.top());
             right.pop();
-        }        
+        }
+        if((int)left.size() - (int)right.size() >= 2){
+            right.push(left.top());
+            left.pop();
+        }
+        
     }
     
     double findMedian() {
-      if(left.size() > right.size())
-          return left.top();
-      if(left.size() < right.size())
-          return right.top();
+        if(left.size() > right.size())
+            return left.top();
+        if(right.size() > left.size())
+            return right.top();
         
-        return (left.top() + right.top())/2.00;
+        return (left.top() + right.top())/2.0;
     }
 };
 
