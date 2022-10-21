b1e69bd0e56f584083130874b6693bccd45e630b
Kunal Bisht <bishtkunal40@gmail.com>
2022-10-22 01:33:54 +0530

    Time: 30 ms (71.89%), Space: 17.2 MB (36.10%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        int maxTravelled = -1;
         int n = (int)nums.size();
-        vector<int> dp(n, 1e9);
-        dp[n - 1] = 0;
-        for(int i = n - 2; i >= 0; i--){
-            for(int j = 1; j <= nums[i] && i + j < n; j++){
-                dp[i] = min(dp[i], dp[i + j] + 1);
+        vector<int> dp(n);
+        
+        for(int i = 0; i < n; i++){
+            int lower_bound = i + 1;
+            int upper_bound = min(n - 1, i + nums[i]);
+            
+            if(lower_bound <= maxTravelled){
+                lower_bound = maxTravelled + 1;
             }
+            while(lower_bound <= upper_bound){
+                dp[lower_bound] = dp[i] + 1;
+                lower_bound++;
+            }
+            maxTravelled = max(maxTravelled, upper_bound);
         }
-        return dp[0];
+        return dp[n - 1];
     }
 };
